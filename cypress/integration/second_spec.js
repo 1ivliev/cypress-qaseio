1a0d6ce05639280e219a914761c2181f040e791e
Kirill Ivliev <1ivliev@mail.ru>
2022-06-12 08:10:32 +0300

    upd

--- cypress/integration/second_spec.js
+++ cypress/integration/second_spec.js
@@ -2,15 +2,7 @@ import { qase } from 'cypress-qase-reporter/dist/mocha';
 
 describe('My First Test', () => {
     qase(2, it('Gets, types and asserts', () => {
-        cy.visit('https://example.cypress.io');
-
-        cy.contains('type').click();
-        
-        // Should be on a new URL which includes '/commands/actions'
-        cy.url().should('include', '/commands/actions');
-        // Get an input, type into it and verify that the value has been updated
-        cy.get('.action-email')
-            .type('fake@email.com')
-            .should('have.value', 'unexpected@email.com');
+        cy.visit('https://qase.io');
+        cy.get('h1').contains('type')
     }));
 });
